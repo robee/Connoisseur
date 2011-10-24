ebb7f9df8df4893f9136205485fb6ee70abdbb68
Ross Robinson <mr.rossrobinson@gmail.com>
2011-10-23 20:51:28 -0400

    - more changes to the app - debugging

--- connbackend/app.py
+++ connbackend/app.py
@@ -14,16 +14,20 @@ from copy import deepcopy
 from models import Menu, MenuItem, UIProfile, Restaurant
 from constants import *
 
+AUTH_ENABLED = False
+PASSCODE = 'aaaaa'
+
+
 """
 {
-     "menu_id": "09jv903rjv",
-     "restaurant_id":"34534f34f",
-     "restaurant_name":"NAME",
-     
-     "menu_name": "Full Menu",
-     "template": "classy",
+     "menu_id": "%s",
+     "restaurant_id":"%s",
+     "restaurant_name":"%s",
+     "menu_name": "%s",
      "ui_profile":
      {
+         "profile_id": "3454gse",
+         "template": "classy",
          "color": "red",
          "font": "helvetica"
          "logo_url": "http://i.imgur.com/eCcSx.jpg",
@@ -66,8 +70,8 @@ from constants import *
           ]  
      }
  }
-
-
+"""
+"""
 
  class Restaurant(db.Model):
      restaurant_id = db.StringProperty(required=True)
@@ -80,7 +84,6 @@ from constants import *
  class Menu(db.Model):
      menu_id = db.StringProperty(required=True)
      name = db.StringProperty(required=True)
-     template = db.StringProperty()
      restaurant = db.ReferenceProperty(Restaurant, required=True)
 
      def get_by_id(m_id):
@@ -104,41 +107,100 @@ from constants import *
 
  class UIProfile(db.Model):
      profile_id = db.StringProperty(required=True)
+     template = db.StringProperty()
      menu = db.ReferenceProperty(Menu, required=True)
      color = db.StringProperty()
      font = db.StringProperty()
 
 """
 
-def DocFromModels(rest_id, menu_id):
-    
-    rest = Restaurant.get_by_id(rest_id)
-    menu = Menu.get_by_id(menu_id)
-    ui_profile = UIProfile.get_by_menu(menu)
-    menuitems = MenuItem.get_by_menu(menu)
-    
-    
-    obj = {}
-    obj['menu_id']=menu.menu_id
-    obj['restaurant_id']=rest.restaurant_id
-    obj['restaurant_name']=rest.name
-    obj['menu_name']= menu.name
-    obj['ui_profile']= deepcopy(ui_profile.vars())
-    obj['menuitems']={}
-    
-    for menuitem in menuItems:
-        category = menuitem.category
-        obj['menuitems'][category] = list(obj['menuitems'][category]).append(menuitem)
-    
-    return json.dumps(obj)
-    
 
 def MenuFromDoc(jsonString):
-    obj = json.loads(jsonString)
-    menu_id = obj['menu_id']
-    rest_id = obj['rest_id']
+    try:
+        obj = json.loads(jsonString)
+        menu_id = obj['menu_id']
+        rest_id = obj['rest_id']
+        profile_id = obj['ui_profile']['profile_id']
     
-    menu = Menu.get_by_+id
+        # Create or Update Rest
+        rest = Restaurant.get_by_id(rest_id)
+        rest.name = obj['restaurant_name']
+    
+        #Create or Update Menu
+        menu = Menu.get_by_id(menu_id)
+    
+        menu.name = obj['menu_name']
+        menu.resturant = rest
+    
+        #Create or Update UI Profile
+        ui_profile = UIProfile.get_by_id(profile_id)
+        ui_profile.template = obj['ui_profile']['template']
+        ui_profile.color = obj['ui_profile']['color']
+        ui_profile.font = obj['ui_profile']['font']
+        ui_profile.logo_url = obj['ui_profile']['logo_url']
+    
+        #Create or Update menuitems
+               # 
+               # for category in obj['menuitems']
+               #     category_name = category.key
+               #     for menu_item_dict in category:
+               #         menuitem = MenuItem.get_by_id(menu_item_dict['menuitem_id'])
+               #         menuitem.menu = menu
+               #         menuitem.name = menu_item_dict['name']
+               #         menuitem.category = category_name
+               #         menuitem.price = menu_item_dict['price']
+               #         menuitem.image = menu_item_dict['image']
+               #         menuitem.description = menu_item_dict['description']
+               #         menuitem.put()
+        ui_profile.put()
+        menu.put()
+        rest.put()
+        return True
+    except:
+        return False
+
+def DocFromModels(rest_id, menu_id):
+    #try:
+        rest = Restaurant.get_by_id(rest_id)
+        menu = Menu.get_by_id(menu_id)
+        ui_profile = UIProfile.get_by_menu(menu)
+        menuitems = MenuItem.get_by_menu(menu)
+       
+        grab_vars = lambda item:  deepcopy(vars(item)['_entity'])
+        
+        
+        obj = {}
+        obj['menu_id']=menu.menu_id
+        obj['restaurant_id']=rest.restaurant_id
+        obj['restaurant_name']=rest.name
+        obj['menu_name']= menu.name
+        obj['ui_profile']= grab_vars(ui_profile)
+        obj['ui_profile']['menu']=None
+        obj['menuitems']={}
+    
+        for menuitem in menuitems:
+            category = menuitem.category
+            menu_item_dict = grab_vars(menuitem)
+            menu_item_dict['menu']=None
+            if obj['menuitems'].has_key(category):
+                obj['menuitems'][category] = list(obj['menuitems'][category]).append(menu_item_dict)
+            else:
+                obj['menuitems'][category] = [menu_item_dict]
+    
+        return json.dumps(obj)
+        #return str(obj)
+    #except:
+    #    return 'ERROR: Invalid restaurant_id or menu_id'
+
+def verifyMessage( request ):
+    #message, secret_key, timestamp, request_hash
+    message = request.get('doc')
+    timestamp = request.get('timestamp')
+    request_hash = request.get('message_hash')
+    doc = json.loads(message)
+    secret_key = Restaurant.get_by_id(doc['restaurant_id']).secret_key
+    message_hash = hashlib.sha1( message+secret_key+timestamp ).hexdigest(  )
+    return message_hash==request_hash
     
 
 # Home Page Handler
@@ -152,69 +214,98 @@ class MainHandler(webapp.RequestHandler):
         self.response.out.write(template.render('templates/index.html', content_template_values))
 
 
-# Home Page Handler
-class UpdateMenu(webapp.RequestHandler):
-    def get(self):
 
-        content_template_values = {
+class Update(webapp.RequestHandler):
+    def post(self):
+        if AUTH_ENABLED and not verifyMessage(self): 
+            self.response.out.write('AUTH FAILED')
+            return
+        message = request.get('doc')
+        if MenuFromDoc(message):
+            self.response.out.write('Update Successful')
+        else:
+            self.response.out.write('ERROR: Incomplete or Malformed doc')
 
-        }
 
-        self.response.out.write(template.render('templates/index.html', content_template_values))
-# Home Page Handler
+# curl -d "restaurant_id=8a72d9b62ad84d1d9c8f9927eea8e07c&menu_name=NEWMENU" http://localhost:8087/menu/create        
 class CreateMenu(webapp.RequestHandler):
     def post(self):
-
-        content_template_values = {
-
-        }
-
-        self.response.out.write(template.render('templates/index.html', content_template_values))
-
-# Home Page Handler
+        if AUTH_ENABLED and not verifyMessage(self): 
+            self.response.out.write('AUTH FAILED')
+            return
+        rest_id = self.request.get('restaurant_id')
+        menu_name = self.request.get('menu_name')
+        
+        rest = Restaurant.get_by_id(rest_id)
+        if not rest:  
+            self.response.out.write('Invalid restaurant_id')
+            return
+            
+        menu = Menu.create(menu_name, rest)
+        uiProfile = UIProfile.create(menu)
+        menu_item_1 = MenuItem.create('Starter Item 1', menu, 10.00, 'Appy', 'This is a sample menu Item')
+        menu_item_2 = MenuItem.create('Starter Item 2', menu, 11.00, 'Drink','This is a sample menu Item')
+        
+        self.response.out.write(DocFromModels(rest_id, menu.menu_id))
+        
 class DeleteMenu(webapp.RequestHandler):
     def post(self):
+        if AUTH_ENABLED and not verifyMessage(self): 
+            self.response.out.write('AUTH FAILED')
+            return
+        message = self.request.get('doc')
+        doc = json.loads(message)
+        if Menu.delete_by_id(doc['menu_id']):
+            self.response.out.write('Menu Successfully Deleted')
+        else:
+            self.response.out.write('ERROR: Could not delete')
 
-        content_template_values = {
-
-        }
-
-        self.response.out.write(template.render('templates/index.html', content_template_values))
-
-
-# Home Page Handler
-class UpdateRestaurant(webapp.RequestHandler):
+class DeleteRestaurant(webapp.RequestHandler):
     def post(self):
+        if AUTH_ENABLED and not verifyMessage(self): 
+            self.response.out.write('AUTH FAILED')
+            return
+        message = request.get('doc')
+        doc = json.loads(message)
+        if Restaurant.delete_by_id(doc['menu_id']):
+            self.response.out.write('Menu Successfully Deleted')
+        else:
+            self.response.out.write('ERROR: Could not delete')
 
-        content_template_values = {
-
-        }
-
-        self.response.out.write(template.render('templates/index.html', content_template_values))
-        
-# Home Page Handler
+# curl -d "name=TEST" http://localhost:8087/restaurant/create
 class CreateRestaurant(webapp.RequestHandler):
     def post(self):
-        message_hash = hashlib.sha1( message+secret_key+message_key ).hexdigest(  )
-        
-        content_template_values = {
-
+        auth_code = self.request.get('auth_code')
+        if auth_code != PASSCODE and AUTH_ENABLED: 
+            self.response.out.write("AUTH FAILED")
+            return
+            
+        rest = Restaurant.create(self.request.get('name'))
+        return_obj = {
+            'secret_key':rest.secret_key,
+            'restaurant_id':rest.restaurant_id
         }
-
-        self.response.out.write(template.render('templates/index.html', content_template_values))
-
-
-
-
-
-
+        
+        self.response.out.write(json.dumps(return_obj))
+        
+class GetMenu(webapp.RequestHandler):
+    def get(self):
+        if AUTH_ENABLED and not verifyMessage(self): 
+            self.response.out.write('AUTH FAILED')
+            return
+        message = request.get('doc')
+        doc = json.loads(message)
+        rest_id = doc['restaurant_id']
+        menu_id = doc['menu_id']
+        self.response.out.write(DocFromModels(rest_id, menu_id))
+        
 
 appRoute = webapp.WSGIApplication( [    ('/restaurant/create',  CreateRestaurant),
-										('/restaurant/update',  UpdateRestaurant),
+										('/restaurant/delete',  DeleteRestaurant),
 										('/menu/create',        CreateMenu),
-										('/menu/update',        UpdateMenu),
+										('/a/update',             Update),
 										('/menu/delete',        DeleteMenu),
-										('/menu/',              Menu),
+										('/menu/',              GetMenu),
 										('/', MainHandler)
 										], debug=True)
 										
--- connbackend/models.py
+++ connbackend/models.py
@@ -8,37 +8,76 @@ from google.appengine.ext import db
 
 class Restaurant(db.Model):
     restaurant_id = db.StringProperty(required=True)
+    secret_key = db.StringProperty(required=True)
     name = db.StringProperty(required=True)
 
     @staticmethod
-    def get_by_id(r_id):
-        return Restaurant.all().filter('restaurant_id =', r_id).get()
+    def create(name):
+        rest = Restaurant(rest)
         
     @staticmethod
-    def save(name_p, menu_p=None):
-        rest = Restaurant(restaurant_id = uuid.uuid4().hex[:16], name=name_p, menus = menu_p)
+    def get_by_id(r_id):
+        return Restaurant.all().filter('restaurant_id =', r_id).get()
+    
+    @staticmethod
+    def delete_by_id(r_id):
+        
+        rest = Restaurant.get_by_id(r_id)
+        
+        try:
+            rest.delete()
+        except:
+            return False
+        menus = Menu.get_menus_by_rest_id(r_id)
+        for menu in menus:
+            Menu.delete_by_id(menu.menu_id)
+        
+        return True
+    
+    @staticmethod
+    def create(name_p, menu_p=None):
+        rest = Restaurant(restaurant_id = uuid.uuid4().hex, secret_key=uuid.uuid4().hex, name=name_p, menus = menu_p)
         rest.put()
+        return rest
     
 class Menu(db.Model):
     menu_id = db.StringProperty(required=True)
     name = db.StringProperty(required=True)
-    template = db.StringProperty()
     restaurant = db.ReferenceProperty(Restaurant, required=True)
 
 
     @staticmethod
     def get_by_id(m_id):
         return Menu.all().filter('menu_id =', m_id).get()
-
+    
+    @staticmethod
+    def delete_by_id(m_id):
+        menu = Menu.get_by_id(m_id)
+        
+        
+        menuitems = MenuItem.get_by_menu(menu)
+        for menuitem in menuitems:
+            menuitem.delete()
+        
+        ui_profile = UIProfile.get_by_menu(menu)
+        UIProfile.delete_by_id(ui_profile.profile_id)
+        try:
+            menu.delete()
+        except:
+            return False
+        
+        return True
+    
     @staticmethod
     def get_menus_by_rest_id(rest_id):
         rest = Restaurant.get_by_id(rest_id)
         return Menu.all.filter('restaurant =', rest).fetch(100)
         
     @staticmethod
-    def save(name_p, restaurant_p, template_p="", ):
-        menu = Menu(menu_id = uuid.uuid4().hex[:16], name=name_p, template = template_p, restaurant=restaurant_p)
+    def create(name_p, restaurant_p):
+        menu = Menu(menu_id = uuid.uuid4().hex[:16], name=name_p, restaurant=restaurant_p)
         menu.put()
+        return menu
 
 
 
@@ -54,25 +93,60 @@ class MenuItem(db.Model):
     
     @staticmethod
     def get_by_menu(menu):
-        return MenuItems.all().filter('menu =', menu).fetch(1000)
-    
+        return MenuItem.all().filter('menu =', menu).fetch(1000)
+        
+    @staticmethod
+    def delete_by_id(i_id):
+        menuitem = MenuItem.get_by_id(i_id)
+        
+        try:
+            menuitem.delete()
+        except:
+            return False
+        
+        return True
+        
     @staticmethod
     def get_by_id(i_id):
         return Menu.all().filter('menuitem_id =', i_id).get()
         
     @staticmethod
-    def save(name_p, menu_p, category_p="", price_p=100.0, image_p="", description_p=""):
-        menuitem = Menu(menuitem_id = uuid.uuid4().hex[:16], menu=menu_p, name=name_p, price = price_p, image = image_p, description=description_p, category=category_p)
+    def create(name_p, menu_p, price_p=100.0, category_p="",  image_p="", description_p=""):
+        menuitem = MenuItem(menuitem_id = uuid.uuid4().hex[:16], menu=menu_p, name=name_p, price = price_p, image = image_p, description=description_p, category=category_p)
         menuitem.put()
+        return menuitem
 
 
 class UIProfile(db.Model):
     profile_id = db.StringProperty(required=True)
     menu = db.ReferenceProperty(Menu, required=True)
+    template = db.StringProperty()
     color = db.StringProperty()
     font = db.StringProperty()
     logo_url = db.StringProperty()
-   
+    
+    @staticmethod
+    def create(menu_p, template_p='classy', color_p='black', font_p='Helvetica', logo_url_p='http://www.virginialogos.com/Portals/57ad7180-c5e7-49f5-b282-c6475cdb7ee7/Food.jpg'):
+        ui_profile = UIProfile(profile_id=uuid.uuid4().hex[:16], menu=menu_p, template=template_p, color=color_p, font=font_p, logo_url=logo_url_p)
+        ui_profile.put()
+        return ui_profile
+    
+    @staticmethod
+    def delete_by_id(p_id):
+        profile_id = UIProfile.get_by_id(p_id)
+        
+        try:
+            profile_id.delete()
+        except:
+            return False
+        
+        return True
+        
+        
+    @staticmethod
+    def get_by_id(profile_id):
+        return UIProfile.all().filter('profile_id =', profile_id).get()
+        
     @staticmethod
     def get_by_menu(menu):
         return UIProfile.all().filter('menu =', menu).get()
